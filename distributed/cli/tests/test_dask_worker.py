2c19bd26b8a2df078da8c7050ee18660693f6c9e
James Bourbeau <jrbourbeau@gmail.com>
2021-09-28 14:59:27 -0500

    Same for dask-worker test

--- distributed/cli/tests/test_dask_worker.py
+++ distributed/cli/tests/test_dask_worker.py
@@ -505,7 +505,7 @@ def dask_setup(worker):
 """
     async with Scheduler(port=0) as s:
         async with Client(s.address, asynchronous=True) as c:
-            env = {"DASK_DISTRIBUTED__WORKER__PRELOAD": preload_text}
+            env = {**os.environ, **{"DASK_DISTRIBUTED__WORKER__PRELOAD": preload_text}}
             with popen(
                 [
                     "dask-worker",
